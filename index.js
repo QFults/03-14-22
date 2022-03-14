5fb43f5257a2f301929ac9d170ff84ca11c846c5
Quinton Fults <quinton.fults.work@gmail.com>
2022-03-14 13:56:14 -0700

    code

--- index.js
+++ index.js
@@ -39,17 +39,46 @@
 
 // console.log(5 / 7)
 
-class Rectangle {
-  constructor (width, height) {
-    this.width = width
-    this.height = height
-  }
-  printInfo () {
-    console.log(`Width: ${this.width}cm | Height: ${this.height}cm`)
+// class Rectangle {
+//   constructor (width, height) {
+//     this.width = width
+//     this.height = height
+//   }
+//   printInfo () {
+//     console.log(`Width: ${this.width}cm | Height: ${this.height}cm`)
+//   }
+// }
+
+// const shape = new Rectangle(500, 900)
+
+// // console.log(shape)
+// shape.printInfo()
+
+class Person {
+  constructor (name, email, phone) {
+    this.name = name
+    this.email = email
+    this.phone = phone
+    this.university = 'UC Irvine'
   }
 }
 
-const shape = new Rectangle(500, 900)
+class Student extends Person {
+  constructor (name, email, phone, grade) {
+    super(name, email, phone)
+    this.grade = grade
+  }
+}
 
-// console.log(shape)
-shape.printInfo()
+class Teacher extends Person {
+  constructor (name, email, phone, subject) {
+    super(name, email, phone)
+    this.subject = subject
+  }
+}
+
+const student = new Student('Sean Cruz', 'seancruz@gmail.com', 1234567890, 100)
+const teacher = new Teacher('Quinton Fults', 'quintonfults@gmail.com', 5555555555, 'Coding')
+
+console.log(student)
+console.log(teacher)
